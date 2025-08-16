a3ecc670428e7098c78a4cffde8b1ff5abe1fd5f
BaiMaWangchuk <bmwangchuk@192.168.1.10>
2025-08-16 20:22:59 +0800

    更新优化功能模块

--- T11_藏字构件动态统计/Dynamic-Component-Statistics.py
+++ T11_藏字构件动态统计/Dynamic-Component-Statistics.py
@@ -17,8 +17,6 @@ from ttkbootstrap import Style
 from ttkbootstrap.constants import *
 import sys
 
-
-# 合并的Creat_18785Componet代码
 # 30个辅音
 consonant = ['\u0F40', '\u0F41', '\u0F42', '\u0F44', '\u0F45', '\u0F46', '\u0F47', '\u0F49',
              '\u0F4F', '\u0F50', '\u0F51', '\u0F53', '\u0F54', '\u0F55', '\u0F56', '\u0F58',
